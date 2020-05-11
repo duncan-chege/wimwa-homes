2c5008ad7799d0a54e6b22a46ba0bbb1d50a0147
dunyung1 <dshege4@gmail.com>
2020-05-11 15:58:33 +0300

    add all images to gallery page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
-/* ---------------------------
-		JS
- --------------------------- */
+const tabs = document.querySelectorAll('[data-tab-target]'); //buttons
+const tabContents = document.querySelectorAll('[data-tab-content]'); //images per button
+
+tabs.forEach(tab => {
+	tab.addEventListener('click', () =>{
+		const target = document.querySelector(tab.dataset.tabTarget);
+		tabContents.forEach(tabContent => tabContent.classList.remove('active'));
+		tabs.forEach(tab => tab.classList.remove('active'));
+		tab.classList.add('active');
+		target.classList.add('active');
+	});
+});
