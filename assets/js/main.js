ba3ae3a264ca09c17a3fb864ff36b8bec07431bf
dunyung1 <dshege4@gmail.com>
2020-05-22 20:44:00 +0300

    change hover nav effects

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ const tabs = document.querySelectorAll('[data-tab-target]'); //buttons
 const tabContents = document.querySelectorAll('[data-tab-content]'); //images per button
 
 tabs.forEach(tab => {
-	tab.addEventListener('click', () =>{
+	tab.addEventListener('click', () => {
 		const target = document.querySelector(tab.dataset.tabTarget);
 		tabContents.forEach(tabContent => tabContent.classList.remove('active'));
 		tabs.forEach(tab => tab.classList.remove('active'));
